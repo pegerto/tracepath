956810d8ac8b4482f6c0433d2aa739cb49682aeb
Pegerto <pegerto@gmail.com>
2014-09-05 18:54:52 +0200

    - Implementing and testing a ping

--- tracepath.py
+++ tracepath.py
@@ -2,6 +2,9 @@ __author__ = 'pegerto@gmail.com'
 
 import socket
 import struct
+import random
+import time
+import argparse
 
 ICMP_ECHO_REQUEST = 8
 ICMP_CODE = socket .getprotobyname('icmp')
@@ -26,10 +29,40 @@ def checksum(package):
     return (answer >> 8) | (answer << 8 & 0xff00)
 
 
-
-def create_packet(id):
+def create_packet(id, dbytes):
     header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,0,id,1)
-    data = 192 * 'Q'
+    data = dbytes * 'P'
     pkg_checksum = checksum(header+data)
     return struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, socket.htons(pkg_checksum), id, 1) + data
+
+
+def ping(dest,dbytes):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
+        packet_id = int(random.random() % 65535)
+        packet = create_packet(packet_id, dbytes)
+
+        sent = sock.sendto(packet, (dest, 1))
+        sent_time = time.time()
+        rec_packet, addr = sock.recvfrom(1024)
+        recv_time = time.time()
+
+        print recv_time - sent_time
+
+    except socket.error as e:
+        print "ERROR: " + e.strerror
+
+    except Exception as e:
+        print "ERROR: " + e.message
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Dynamically discover the PMTU of a path')
+    parser.add_argument('ip',type=str, help='Destination ip')
+    args = parser.parse_args()
+    ping(args.ip, 40)
+
+
+if __name__ == '__main__':
+    main()
 
